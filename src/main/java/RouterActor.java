1c7995b9fe72f97b554c03d854e53fcc6a2a25f3
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-21 21:54:37 +0000

    Scripted auto-commit on change (2019-11-21 21:54:37) by gitwatch.sh

--- src/main/java/RouterActor.java
+++ src/main/java/RouterActor.java
@@ -1,4 +1,6 @@
 import akka.actor.*;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 import akka.japi.pf.DeciderBuilder;
 import scala.concurrent.duration.Duration;
 
@@ -7,6 +9,7 @@ import static akka.actor.SupervisorStrategy.escalate;
 
 public class RouterActor {
     private static final int MAX_RETRIES = 10;
+    LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
 
     ActorSystem system = ActorSystem.create("test");
     ActorRef routerActor = system.actorOf(
