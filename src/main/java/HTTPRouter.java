f3a628508dbb8ed3acc323070ad40ecadcf7d399
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-21 21:41:28 +0000

    Scripted auto-commit on change (2019-11-21 21:41:28) by gitwatch.sh

--- src/main/java/HTTPRouter.java
+++ src/main/java/HTTPRouter.java
@@ -1,13 +1,20 @@
 import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
 import akka.http.javadsl.server.Route;
+
+import java.time.Duration;
 
 import static akka.http.javadsl.server.Directives.*;
 
 public class HTTPRouter {
 
     final private ActorRef router;
+    private final Duration askTimeout;
+    private final Scheduler scheduler;
 
-    public HTTPRouter(ActorRef router) {
+    public HTTPRouter(ActorSystem system, ActorRef router) {
+        scheduler = system.scheduler();
+        askTimeout = system.settings().config().getDuration("my-app.routes.ask-timeout");
         this.router = router;
     }
 
