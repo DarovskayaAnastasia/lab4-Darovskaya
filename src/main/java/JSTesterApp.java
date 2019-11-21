ea36494e51ebef181908253d6cf5e8bc668552da
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-21 16:44:12 +0000

    Scripted auto-commit on change (2019-11-21 16:44:12) by gitwatch.sh

--- src/main/java/JSTesterApp.java
+++ src/main/java/JSTesterApp.java
@@ -32,15 +32,16 @@ public class JSTesterApp {
 
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = jsTesterApp.createRoute().flow(classicSystem, materializer);
 
-        CompletionStage<ServerBinding> futureBinding =
+        final CompletionStage<ServerBinding> binding =
                 http.bindAndHandle(routeFlow,
                         ConnectHttp.toHost("localhost", 8080),
-                        materializer);
+                        materializer
+                );
 
-        System.out.println();
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
 
         System.in.read();
-        futureBinding
+        binding
                 .thenCompose(ServerBinding::unbind)
                 .thenAccept(unbound -> classicSystem.terminate());
     }
