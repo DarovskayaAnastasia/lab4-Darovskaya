fabd85b734f2205320701c234def582ac9ddf4d2
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-21 23:21:26 +0000

    Scripted auto-commit on change (2019-11-21 23:21:26) by gitwatch.sh

--- src/main/java/TestRunnerActor.java
+++ src/main/java/TestRunnerActor.java
@@ -8,6 +8,7 @@ import akka.japi.pf.DeciderBuilder;
 import akka.japi.pf.ReceiveBuilder;
 import scala.concurrent.duration.Duration;
 
+import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
@@ -22,6 +23,10 @@ public class TestRunnerActor extends AbstractActor {
 
     private static test() {
         ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        ScriptEngineManager().getEngineByName("nashorn");
+        engine.eval(jscript);
+        Invocable invocable = (Invocable) engine;
+        return invocable.invokeFunction(functionName, params).toString();
     }
     @Override
     public Receive createReceive() {
