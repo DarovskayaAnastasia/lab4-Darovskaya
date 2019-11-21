794bc2c7c16344c31e9a94d9ac4cda0165990206
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-21 23:00:45 +0000

    Scripted auto-commit on change (2019-11-21 23:00:45) by gitwatch.sh

--- src/main/java/RouterActor.java
+++ src/main/java/RouterActor.java
@@ -45,6 +45,6 @@ public class RouterActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(TestCasesClass.class, )
+                .match(SomeName.class, )
     }
 }
