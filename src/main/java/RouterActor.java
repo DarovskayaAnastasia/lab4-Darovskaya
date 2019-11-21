9986f683326d8647ecfc7905928c5821c5f854c3
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-21 21:34:52 +0000

    Scripted auto-commit on change (2019-11-21 21:34:52) by gitwatch.sh

--- src/main/java/RouterActor.java
+++ src/main/java/RouterActor.java
@@ -1,2 +1,24 @@
+import akka.actor.OneForOneStrategy;
+import akka.actor.SupervisorStrategy;
+import akka.japi.pf.DeciderBuilder;
+import scala.concurrent.duration.Duration;
+
+import static akka.actor.SupervisorStrategy.*;
+import static akka.actor.SupervisorStrategy.escalate;
+
 public class RouterActor {
+    private static final int MAX_RETRIES = 10;
+
+    private static SupervisorStrategy strategy =
+            new OneForOneStrategy(MAX_RETRIES,
+                    Duration.create("1 minute"),
+                    DeciderBuilder.
+                            match(ArithmeticException.class, e -> resume()).
+                            match(NullPointerException.class, e -> restart()).
+                            match(IllegalArgumentException.class, e -> stop()).
+                            matchAny(o -> escalate()).build());
+    @Override
+    public SupervisorStrategy supervisorStrategy() {
+        return strategy;
+    }
 }
--- src/main/java/TestRunnerActor.java
+++ src/main/java/TestRunnerActor.java
@@ -1,12 +1,22 @@
 import akka.actor.AbstractActor;
 import akka.actor.OneForOneStrategy;
 import akka.actor.SupervisorStrategy;
+import akka.japi.pf.DeciderBuilder;
+import scala.concurrent.duration.Duration;
+
+import static akka.actor.SupervisorStrategy.*;
 
 public class TestRunnerActor extends AbstractActor {
         private static final int MAX_RETRIES = 10;
-...
+
         private static SupervisorStrategy strategy =
                 new OneForOneStrategy(MAX_RETRIES,
+                        Duration.create("1 minute"),
+                        DeciderBuilder.
+                                match(ArithmeticException.class, e -> resume()).
+                                match(NullPointerException.class, e -> restart()).
+                                match(IllegalArgumentException.class, e -> stop()).
+                                matchAny(o -> escalate()).build());
         @Override
         public SupervisorStrategy supervisorStrategy() {
             return strategy;
