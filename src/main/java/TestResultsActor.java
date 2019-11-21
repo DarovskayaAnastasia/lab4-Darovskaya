a7add33c675176ca70d780512b8935f47da5c82b
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-21 21:35:11 +0000

    Scripted auto-commit on change (2019-11-21 21:35:11) by gitwatch.sh

--- src/main/java/TestResultsActor.java
+++ src/main/java/TestResultsActor.java
@@ -1,8 +1,27 @@
 import akka.actor.AbstractActor;
+import akka.actor.OneForOneStrategy;
+import akka.actor.SupervisorStrategy;
+import akka.japi.pf.DeciderBuilder;
+import scala.concurrent.duration.Duration;
+
+import static akka.actor.SupervisorStrategy.*;
+import static akka.actor.SupervisorStrategy.escalate;
 
 public class TestResultsActor extends AbstractActor {
-    private Map<String, >
+    private static final int MAX_RETRIES = 10;
 
+    private static SupervisorStrategy strategy =
+            new OneForOneStrategy(MAX_RETRIES,
+                    Duration.create("1 minute"),
+                    DeciderBuilder.
+                            match(ArithmeticException.class, e -> resume()).
+                            match(NullPointerException.class, e -> restart()).
+                            match(IllegalArgumentException.class, e -> stop()).
+                            matchAny(o -> escalate()).build());
+    @Override
+    public SupervisorStrategy supervisorStrategy() {
+        return strategy;
+    }
     @Override
     public AbstractActor.Receive createReceive() {
         return receiveBuilder()
