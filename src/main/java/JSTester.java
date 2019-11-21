498b7dd799cbc810fea9357f682e5708a273515c
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-21 14:20:35 +0000

    Scripted auto-commit on change (2019-11-21 14:20:35) by gitwatch.sh

--- src/main/java/JSTester.java
+++ src/main/java/JSTester.java
@@ -1,2 +1,16 @@
-public class JSTester {
+import akka.actor.AbstractActor;
+
+public class JSTester extends AbstractActor {
+//    какие компоненты будут проверяться, например
+//    в примере это сообщение. В переменную  GREET кладется приветствие
+//    done отправляется ...
+//    public static enum Msg {
+//        GREET, DONE;
+//    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .
+    }
 }
