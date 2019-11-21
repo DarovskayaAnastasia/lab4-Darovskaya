99197e5a332c2868ba1f972877a7add7d2af78c5
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-21 20:51:14 +0000

    Scripted auto-commit on change (2019-11-21 20:51:14) by gitwatch.sh

--- src/main/java/HTTPRouter.java
+++ src/main/java/HTTPRouter.java
@@ -1,4 +1,5 @@
 import akka.actor.ActorRef;
+import akka.http.javadsl.server.Route;
 
 public class HTTPRouter {
 
@@ -8,6 +9,30 @@ public class HTTPRouter {
         this.router = router;
     }
 
-    
+    private Route func() {
+        route(
+                path("semaphore", () ->
+                        route(
+                                get( () -> {
+                                    Future<Object> result = Patterns.ask(testPackageActor,
+                                            SemaphoreActor.makeRequest(), 5000);
+                                    return completeOKWithFuture(result, Jackson.marshaller());
+                                }))),
+                path("test", () ->
+                        route(
+                                post(() ->
+                                        entity(Jackson.unmarshaller(TestPackageMsg.class), msg -> {
+                                            testPackageActor.tell(msg, ActorRef.noSender());
+                                            return complete("Test started!");
+                                        })))),
+                path("put", () ->
+                        get(() ->
+                                parameter("key", (key) ->
+                                        parameter("value", (value) ->
+                                        {
+                                            storeActor.tell(new StoreActor.StoreMessage(key, value), ActorRef.noSender());
+                                            return complete("value saved to store ! key=" + key + " value=" + value);
+                                        })))),
+    }
 }
 
