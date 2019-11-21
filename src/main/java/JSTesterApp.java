e042069a267c066dc61a7b75e01c6e84c8391012
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-21 16:19:46 +0000

    Scripted auto-commit on change (2019-11-21 16:19:46) by gitwatch.sh

--- src/main/java/JSTesterApp.java
+++ src/main/java/JSTesterApp.java
@@ -1,8 +1,43 @@
 import akka.NotUsed;
 import akka.actor.AbstractActor;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.Route;
+import akka.stream.Materializer;
+import akka.stream.javadsl.Flow;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.CompletionStage;
 
 public class JSTesterApp extends AbstractActor {
+
+    static void startHttpServer(Route route, ActorSystem<?> system) {
+        // Akka HTTP still needs a classic ActorSystem to start
+        akka.actor.ActorSystem classicSystem = Adapter.toClassic(system);
+        final Http http = Http.get(classicSystem);
+        final Materializer materializer = Materializer.matFromSystem(system);
+
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = route.flow(classicSystem, materializer);
+        CompletionStage<ServerBinding> futureBinding =
+                http.bindAndHandle(routeFlow, ConnectHttp.toHost("localhost", 8080), materializer);
+
+        futureBinding.whenComplete((binding, exception) -> {
+            if (binding != null) {
+                InetSocketAddress address = binding.localAddress();
+                system.log().info("Server online at http://{}:{}/",
+                        address.getHostString(),
+                        address.getPort());
+            } else {
+                system.log().error("Failed to bind HTTP endpoint, terminating system", exception);
+                system.terminate();
+            }
+        });
+    }
+
     public static void main(String[] args) throws Exception {
         //#server-bootstrapping
         Behavior<NotUsed> rootBehavior = Behaviors.setup(context -> {
