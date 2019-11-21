c4423da0bc4613e0de40af0321b528aee3e32ea2
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-21 14:28:32 +0000

    Scripted auto-commit on change (2019-11-21 14:28:32) by gitwatch.sh

--- src/main/java/TestResultsActor.java
+++ src/main/java/TestResultsActor.java
@@ -1,7 +1,14 @@
+import akka.actor.AbstractActor;
+
 public class TestResultsActor {
     public static enum Msg {
         oneTestResult, getOneTestResultRequest
     }
 
+    @Override
+    public AbstractActor.Receive createReceive() {
+        return receiveBuilder()
+                .matchEquals().build();
+    }
 
 }
