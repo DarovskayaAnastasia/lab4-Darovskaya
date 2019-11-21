4c6ebc030af361fa8a550104a62041a3e2bac03d
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-21 23:38:55 +0000

    Scripted auto-commit on change (2019-11-21 23:38:55) by gitwatch.sh

--- src/main/java/TestResultsActor.java
+++ src/main/java/TestResultsActor.java
@@ -11,29 +11,16 @@ import static akka.actor.SupervisorStrategy.escalate;
 public class TestResultsActor extends AbstractActor {
     private static final int MAX_RETRIES = 10;
 
-    private static SupervisorStrategy strategy =
-            new OneForOneStrategy(MAX_RETRIES,
-                    Duration.create("1 minute"),
-                    DeciderBuilder.
-                            match(ArithmeticException.class, e -> resume()).
-                            match(NullPointerException.class, e -> restart()).
-                            match(IllegalArgumentException.class, e -> stop()).
-                            matchAny(o -> escalate()).build());
-    @Override
-    public SupervisorStrategy supervisorStrategy() {
-        return strategy;
-    }
-
     @Override
     public Receive createReceive() {
-        return receiveBuilder()
+        return receiveBuilder().create()
                 .match(StoreMessage.class, m -> {
                     sender().tell(Msg.oneTestResultRequest, self());
                 }).build();
     }
 
     static Props props() {
-        return Props.create(TestRunnerActor.class);
+        return Props.create(TestResultsActor.class);
     }
 
 }
