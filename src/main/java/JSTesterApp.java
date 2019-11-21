1ef3957817ff0182088045f7a603e924f3c13748
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-21 16:20:08 +0000

    Scripted auto-commit on change (2019-11-21 16:20:08) by gitwatch.sh

--- src/main/java/JSTesterApp.java
+++ src/main/java/JSTesterApp.java
@@ -37,21 +37,4 @@ public class JSTesterApp extends AbstractActor {
             }
         });
     }
-
-    public static void main(String[] args) throws Exception {
-        //#server-bootstrapping
-        Behavior<NotUsed> rootBehavior = Behaviors.setup(context -> {
-            ActorRef<UserRegistry.Command> userRegistryActor =
-                    context.spawn(UserRegistry.create(), "UserRegistry");
-
-            UserRoutes userRoutes = new UserRoutes(context.getSystem(), userRegistryActor);
-            startHttpServer(userRoutes.userRoutes(), context.getSystem());
-
-            return Behaviors.empty();
-        });
-
-        // boot up server using the route as defined below
-        ActorSystem.create(rootBehavior, "HelloAkkaHttpServer");
-        //#server-bootstrapping
-    }
 }
