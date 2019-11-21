7fcef090e362022479d0dd6f49a312b0afae0536
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-21 21:34:07 +0000

    Scripted auto-commit on change (2019-11-21 21:34:07) by gitwatch.sh

--- src/main/java/TestRunnerActor.java
+++ src/main/java/TestRunnerActor.java
@@ -1,2 +1,14 @@
-public class TestRunnerActor {
-}
+import akka.actor.AbstractActor;
+import akka.actor.OneForOneStrategy;
+import akka.actor.SupervisorStrategy;
+
+public class TestRunnerActor extends AbstractActor {
+        private static final int MAX_RETRIES = 10;
+...
+        private static SupervisorStrategy strategy =
+                new OneForOneStrategy(MAX_RETRIES,
+        @Override
+        public SupervisorStrategy supervisorStrategy() {
+            return strategy;
+        }
+    }
