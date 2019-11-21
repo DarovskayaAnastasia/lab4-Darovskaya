8ad18eeee3502f4242bd529f653e097f09189eed
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-21 21:42:36 +0000

    Scripted auto-commit on change (2019-11-21 21:42:36) by gitwatch.sh

--- src/main/java/HTTPRouter.java
+++ src/main/java/HTTPRouter.java
@@ -1,5 +1,9 @@
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Scheduler;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.model.StatusCodes;
+import akka.http.javadsl.server.PathMatchers;
 import akka.http.javadsl.server.Route;
 
 import java.time.Duration;
@@ -25,5 +29,58 @@ public class HTTPRouter {
     private Route getResult() {
 
     }
+
+    public Route userRoutes() {
+        return pathPrefix("users", () ->
+                concat(
+                        //#users-get-delete
+                        pathEnd(() ->
+                                concat(
+                                        get(() ->
+                                                onSuccess(getUsers(),
+                                                        users -> complete(StatusCodes.OK, users, Jackson.marshaller())
+                                                )
+                                        ),
+                                        post(() ->
+                                                entity(
+                                                        Jackson.unmarshaller(User.class),
+                                                        user ->
+                                                                onSuccess(createUser(user), performed -> {
+                                                                    log.info("Create result: {}", performed.description);
+                                                                    return complete(StatusCodes.CREATED, performed, Jackson.marshaller());
+                                                                })
+                                                )
+                                        )
+                                )
+                        ),
+                        //#users-get-delete
+                        //#users-get-post
+                        path(PathMatchers.segment(), (String name) ->
+                                concat(
+                                        get(() ->
+                                                        //#retrieve-user-info
+                                                        rejectEmptyResponse(() ->
+                                                                onSuccess(getUser(name), performed ->
+                                                                        complete(StatusCodes.OK, performed.maybeUser, Jackson.marshaller())
+                                                                )
+                                                        )
+                                                //#retrieve-user-info
+                                        ),
+                                        delete(() ->
+                                                        //#users-delete-logic
+                                                        onSuccess(deleteUser(name), performed -> {
+                                                                    log.info("Delete result: {}", performed.description);
+                                                                    return complete(StatusCodes.OK, performed, Jackson.marshaller());
+                                                                }
+                                                        )
+                                                //#users-delete-logic
+                                        )
+                                )
+                        )
+                        //#users-get-post
+                )
+        );
+    }
+    //#all-routes
 }
 
