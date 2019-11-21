e8a4c87bce39f4171751f51bc7884a1a25053550
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-21 21:45:19 +0000

    Scripted auto-commit on change (2019-11-21 21:45:19) by gitwatch.sh

--- src/main/java/RouterActor.java
+++ src/main/java/RouterActor.java
@@ -9,6 +9,15 @@ import static akka.actor.SupervisorStrategy.escalate;
 public class RouterActor {
     private static final int MAX_RETRIES = 10;
 
+    ActorSystem system = ActorSystem.create("test");
+    ActorRef storeActor = system.actorOf(
+            Props.create(StoreActor.class)
+    );
+    storeActor.tell(
+        new StoreActor.StoreMessage("test", "test"),
+        ActorRef.noSender()
+        );
+
     private static SupervisorStrategy strategy =
             new OneForOneStrategy(MAX_RETRIES,
                     Duration.create("1 minute"),
