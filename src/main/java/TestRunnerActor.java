e2bda080f091636ca846163e66860ed03a3a9e8c
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-22 19:07:29 +0000

    Scripted auto-commit on change (2019-11-22 19:07:29) by gitwatch.sh

--- src/main/java/TestRunnerActor.java
+++ src/main/java/TestRunnerActor.java
@@ -21,28 +21,48 @@ public class TestRunnerActor extends AbstractActor {
 
     public TestRunnerActor() {}
 
-    private String runTest(MsgTest m) {
-        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-        TestData test = m.getTests()[0];
-        String testResult;
+//    private String runTest(MsgTest m) {
+//        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+//        TestData test = m.getTests()[0];
+//        String testResult;
+//
+//        try {
+//            engine.eval(m.getJsScript());
+//            Invocable invocable = (Invocable) engine;
+//            Object[] params = test.getParams();
+//            return invocable.invokeFunction(m.getFunctionName(), params).toString();
+//        }
+//        catch (Exception exp) {
+//            return testResult = "ERROR";
+//        }
+//    }
 
-        try {
-            engine.eval(m.getJsScript());
-            Invocable invocable = (Invocable) engine;
-            Object[] params = test.getParams();
-            return invocable.invokeFunction(m.getFunctionName(), params).toString();
-        }
-        catch (Exception exp) {
-            return testResult = "ERROR";
-        }
-    }
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(MsgTest.class, m -> {
-                    String testResult = runTest(m);
+                    ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+                    TestData test = m.getTests()[0];
+                    String testResult;
+
+                    try {
+                        engine.eval(m.getJsScript());
+                        Invocable invocable = (Invocable) engine;
+                        Object[] params = test.getParams();
+                        testResult = invocable.invokeFunction(m.getFunctionName(), params).toString();
+                    }
+                    catch (Exception exp) {
+                        testResult = "ERROR";
+                    }
                     getContext().actorSelection("/user/local_server/store_actor")
-                            .tell(,self());
+                            .tell(new TestResult(
+                                    m.getPackageID(),
+                                    test.getTestName(),
+                                    test.getExpectedResult(),
+                                    testResult,
+                                    testResult.equalsIgnoreCase(test.getExpectedResult()),
+                                    test.getParams()
+                            ), self());
                 })
                 .build();
     }
