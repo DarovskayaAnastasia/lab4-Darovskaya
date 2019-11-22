3fb71de193424f88b9314089e9e20a2117d3581a
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-22 18:47:17 +0000

    Scripted auto-commit on change (2019-11-22 18:47:17) by gitwatch.sh

--- src/main/java/MsgTest.java
+++ src/main/java/MsgTest.java
@@ -1,2 +1,10 @@
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+@JsonAutoDetect
 public class MsgTest {
+
+    private String packageID;
+    private String jsScript;
+    private String funcName;
+    private TestData[] tests;
 }
