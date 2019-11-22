b837c608e14153e8c07094cb9528fe3d7e8c1887
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-22 18:29:42 +0000

    Scripted auto-commit on change (2019-11-22 18:29:42) by gitwatch.sh

--- src/main/java/TestResult.java
+++ src/main/java/TestResult.java
@@ -1,12 +1,23 @@
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonAutoDetect
+@JsonPropertyOrder({"testName", "success", "expectedResult", "params", "result"})
+
 public class TestResult {
+    @JsonIgnore
     private String packageID;
+
+    @JsonProperty
     private String testName;
-//    private String expectedResult;
+    private String expectedResult;
     private boolean success;
+    private Object[] params;
+    private String result;
 
     public TestResult() {}
-//    private Object[] params;
-//    private String result;
 
     public TestResult(String packageID, String testName,
                       String expectedResult, String result,
@@ -14,10 +25,10 @@ public class TestResult {
 
         this.packageID = packageID;
         this.testName = testName;
-//        this.expectedResult = expectedResult;
-//        this.result = result;
+        this.expectedResult = expectedResult;
+        this.result = result;
         this.success = success;
-//        this.params = params;
+        this.params = params;
     }
 
     public String getPackageID() {
@@ -31,32 +42,32 @@ public class TestResult {
     public boolean isSuccessful() {
         return success;
     }
-//
-//    public void setSuccessful(boolean successful) {
-//        success = successful;
-//    }
-//
-//    public String getResult() {
-//        return result;
-//    }
-//
-//    public void setResult(String result) {
-//        this.result = result;
-//    }
-//
-//    public String getExpectedResult() {
-//        return expectedResult;
-//    }
-//
-//    public void setExpectedResult(String expectedResult) {
-//        this.expectedResult = expectedResult;
-//    }
-//
-//    public Object[] getParams() {
-//        return params;
-//    }
-//
-//    public void setParams(Object[] params) {
-//        this.params = params;
-//    }
+
+    public void setSuccessful(boolean successful) {
+        success = successful;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    public void setExpectedResult(String expectedResult) {
+        this.expectedResult = expectedResult;
+    }
+
+    public Object[] getParams() {
+        return params;
+    }
+
+    public void setParams(Object[] params) {
+        this.params = params;
+    }
 }
