06d7eee6018d096f7f52715914f6d2515409d214
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-22 00:58:30 +0000

    Scripted auto-commit on change (2019-11-22 00:58:30) by gitwatch.sh

--- src/main/java/MsgResponse.java
+++ src/main/java/MsgResponse.java
@@ -1,2 +1,25 @@
 public class MsgResponse {
+
+    private String packageID;
+    private TestResult[] results;
+    private boolean success;
+
+    public MsgResponse() {}
+
+    public MsgResponse(String packageID, TestResult[] results) {
+        this.packageID = packageID;
+        this.results = results;
+        this.success = true;
+
+        for (TestResult result : results) {
+            if (!result.isSuccessful()) {
+                this.success = false;
+                break;
+            }
+        }
+    }
+
+    public String getPackageID() {
+        return packageID;
+    }
 }
