1471356aa98f3119a56899e2017a9fc5983e618b
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-22 00:19:33 +0000

    Scripted auto-commit on change (2019-11-22 00:19:33) by gitwatch.sh

--- src/main/java/TestResult.java
+++ src/main/java/TestResult.java
@@ -20,5 +20,51 @@ public class TestResult {
         this.params = params;
     }
 
-    
+    public String getPackageId() {
+        return packageId;
+    }
+
+    public void setPackageId(String packageId) {
+        this.packageId = packageId;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public void setTestName(String testName) {
+        this.testName = testName;
+    }
+
+    public boolean suc() {
+        return success;
+    }
+
+    public void setSuccessful(boolean successful) {
+        success = successful;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    public void setExpectedResult(String expectedResult) {
+        this.expectedResult = expectedResult;
+    }
+
+    public Object[] getParams() {
+        return params;
+    }
+
+    public void setParams(Object[] params) {
+        this.params = params;
+    }
 }
