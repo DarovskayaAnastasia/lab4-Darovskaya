0d892ade45ee157882dbac46dffa3df2f7f6c887
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-22 21:12:47 +0000

    Scripted auto-commit on change (2019-11-22 21:12:47) by gitwatch.sh

--- src/main/java/MsgTest.java
+++ src/main/java/MsgTest.java
@@ -1,52 +1,74 @@
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 
+//import com.fasterxml.jackson.annotation.JsonAutoDetect;
+//
+//@JsonAutoDetect
+//public class MsgTest {
+//
+//    private String packageID;
+//    private String jsScript;
+//    private String funcName;
+//    private TestData[] tests;
+//
+//    public MsgTest() {}
+//
+//    public MsgTest(String packageID, String jsScript,
+//                   String funcName, TestData[] tests) {
+//        this.packageID = packageID;
+//        this.jsScript = jsScript;
+//        this.funcName = funcName;
+//        this.tests = tests;
+//    }
+//
+//    public String getPackageID() {
+//        return packageID;
+//    }
+//
+//    public String getJsScript() {
+//        return jsScript;
+//    }
+//
+//    public String getFunctionName() {
+//        return funcName;
+//    }
+//
+//    public TestData[] getTests() {
+//        return tests;
+//    }
+//
+//    public void setPackageID(String packageID) {
+//        this.packageID = packageID;
+//    }
+//
+//    public void setJsScript(String jsScript) {
+//        this.jsScript = jsScript;
+//    }
+//
+//    public void setFunctionName(String funcName) {
+//        this.funcName = funcName;
+//    }
+//
+//    public void setTests(TestData[] tests) {
+//        this.tests = tests;
+//    }
+//}
 @JsonAutoDetect
 public class MsgTest {
 
-    private String packageID;
     private String jsScript;
-    private String funcName;
-    private TestData[] tests;
 
     public MsgTest() {}
 
-    public MsgTest(String packageID, String jsScript,
-                   String funcName, TestData[] tests) {
-        this.packageID = packageID;
+    public MsgTest(String jsScript) {
         this.jsScript = jsScript;
-        this.funcName = funcName;
-        this.tests = tests;
-    }
-
-    public String getPackageID() {
-        return packageID;
     }
 
     public String getJsScript() {
         return jsScript;
     }
 
-    public String getFunctionName() {
-        return funcName;
-    }
-
-    public TestData[] getTests() {
-        return tests;
-    }
-
-    public void setPackageID(String packageID) {
-        this.packageID = packageID;
-    }
-
     public void setJsScript(String jsScript) {
         this.jsScript = jsScript;
     }
 
-    public void setFunctionName(String funcName) {
-        this.funcName = funcName;
-    }
-
-    public void setTests(TestData[] tests) {
-        this.tests = tests;
-    }
 }
