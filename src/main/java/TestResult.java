8bab863e00b5b0b302cbc57e8908e6792f5b2061
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-22 00:16:04 +0000

    Scripted auto-commit on change (2019-11-22 00:16:04) by gitwatch.sh

--- src/main/java/TestResult.java
+++ src/main/java/TestResult.java
@@ -2,6 +2,7 @@ public class TestResult {
     private String packageID;
     private String testName;
     private String expectedResult;
+    private Object[] params;
     private String result;
     private boolean success;
 
