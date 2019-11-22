531d6433f0cf61f44bf7d6b95fa67353f853fe2a
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-22 00:47:12 +0000

    Scripted auto-commit on change (2019-11-22 00:47:12) by gitwatch.sh

--- src/main/java/TestResultsActor.java
+++ src/main/java/TestResultsActor.java
@@ -22,7 +22,7 @@ public class TestResultsActor extends AbstractActor {
                 })
                 .match(TestResult.class, m -> {
                     List<TestResult> results = store.get(m.getPackageID());
-                    
+                    i
                 }).build();
     }
 
