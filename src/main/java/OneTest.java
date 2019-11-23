1f8927b75fde8eed78176d0053a100e7309b97d3
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-23 07:27:33 +0000

    Scripted auto-commit on change (2019-11-23 07:27:33) by gitwatch.sh

--- src/main/java/OneTest.java
+++ src/main/java/OneTest.java
@@ -1,2 +1,56 @@
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonAutoDetect
 public class OneTest {
+
+    private String packageID;
+    private String jsScript;
+    private String funcName;
+    private TestData test;
+
+    public OneTest() {}
+
+    public OneTest(@JsonProperty("packageID") String packageID,
+                   @JsonProperty("jsScript") String jsScript,
+                   @JsonProperty("funcName") String funcName,
+                   @JsonProperty("tests") TestData test) {
+        this.packageID = packageID;
+        this.jsScript = jsScript;
+        this.funcName = funcName;
+        this.test = test;
+        System.out.println("id = " + packageID + "\n" +  "script = " + jsScript + "\n" +  "function name = " + funcName);
+    }
+
+    public String getPackageID() {
+        return packageID;
+    }
+
+    public String getJsScript() {
+        return jsScript;
+    }
+
+    public String getFuncName() {
+        return funcName;
+    }
+
+    public TestData[] getTests() {
+        return tests;
+    }
+
+    public void setPackageID(String packageID) {
+        this.packageID = packageID;
+    }
+
+    public void setJsScript(String jsScript) {
+        this.jsScript = jsScript;
+    }
+
+    public void setFuncName(String funcName) {
+        this.funcName = funcName;
+    }
+
+    public void setTest(TestData[] test) {
+        this.test = test;
+    }
 }
